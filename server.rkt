f87a8119f6462b3f5763a6575bf01599f7f31017
matheuswogt37 <matheusnotas07@gmail.com>
2025-04-25 19:34:35 -0300

    forumulario e lista alterados

--- server.rkt
+++ server.rkt
@@ -24,9 +24,19 @@
             "CREATE TABLE IF NOT EXISTS avaliacoes (
                id INTEGER PRIMARY KEY,
                site TEXT NOT NULL,
-               eficacia INTEGER,
-               eficiencia INTEGER,
-               satisfacao INTEGER,
+               url TEXT NOT NULL,
+               tipo_usuario TEXT NOT NULL,
+               componente_avaliado TEXT NOT NULL,
+               adequacao INTEGER,
+               descricao INTEGER,
+               explicito INTEGER,
+               consistencia INTEGER,
+               prevencao_erros INTEGER,
+               tolerancia_falhas INTEGER,
+               flexibilidade_eficiencia INTEGER,
+               estetica_design INTEGER,
+               expectativa INTEGER,
+               normas_convencao INTEGER,
                comentarios TEXT,
                avaliado_em DATETIME DEFAULT CURRENT_TIMESTAMP
             );")
@@ -36,11 +46,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Insere uma nova avaliação no banco de dados
-(define (inserir-avaliacao site eficacia eficiencia satisfacao comentarios)
+(define (inserir-avaliacao site url tipo_usuario componente_avaliado adequacao descricao explicito consistencia prevencao_erros tolerancia_falhas flexibilidade_eficiencia estetica_design expectativa normas_convencao comentarios)
   (query-exec db-conn
-              "INSERT INTO avaliacoes (site, eficacia, eficiencia, satisfacao, comentarios)
-               VALUES (?, ?, ?, ?, ?)"
-              site eficacia eficiencia satisfacao comentarios))
+              "INSERT INTO avaliacoes (site, url, tipo_usuario, componente_avaliado, adequacao, descricao, explicito, consistencia, prevencao_erros, tolerancia_falhas, flexibilidade_eficiencia, estetica_design, expectativa, normas_convencao, comentarios)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+              site url tipo_usuario componente_avaliado adequacao descricao explicito consistencia prevencao_erros tolerancia_falhas flexibilidade_eficiencia estetica_design expectativa normas_convencao comentarios))
 
 ;; Retorna as avaliações, opcionalmente filtrando por uma string no campo 'site'
 (define (buscar-avaliacoes [criterio ""])
@@ -60,19 +70,39 @@
 
 (define (gerar-linhas avaliacao-list)
   (for/list ([reg avaliacao-list])
-    (define id           (vector-ref reg 0))
-    (define site         (vector-ref reg 1))
-    (define eficacia     (vector-ref reg 2))
-    (define eficiencia   (vector-ref reg 3))
-    (define satisfacao   (vector-ref reg 4))
-    (define comentarios  (vector-ref reg 5))
-    (define avaliado-em  (vector-ref reg 6))
+    (define id                        (vector-ref reg 0))
+    (define site                      (vector-ref reg 1))
+    (define url                       (vector-ref reg 2))
+    (define tipo_usuario              (vector-ref reg 3))
+    (define componente_avaliado       (vector-ref reg 4))
+    (define adequacao                 (vector-ref reg 5))
+    (define descricao                 (vector-ref reg 6))
+    (define explicito                 (vector-ref reg 7))
+    (define consistencia              (vector-ref reg 8))
+    (define prevencao_erros           (vector-ref reg 9))
+    (define tolerancia_falhas         (vector-ref reg 10))
+    (define flexibilidade_eficiencia  (vector-ref reg 11))
+    (define estetica_design           (vector-ref reg 12))
+    (define expectativa               (vector-ref reg 13))  
+    (define normas_convencao          (vector-ref reg 14))
+    (define comentarios               (vector-ref reg 15))
+    (define avaliado-em               (vector-ref reg 16))
     `(tr
       (td ,(number->string id))
       (td ,site)
-      (td ,(number->string eficacia))
-      (td ,(number->string eficiencia))
-      (td ,(number->string satisfacao))
+      (td ,url)
+      (td ,tipo_usuario)
+      (td ,componente_avaliado)
+      (td ,(number->string adequacao))
+      (td ,(number->string descricao))
+      (td ,(number->string explicito))
+      (td ,(number->string consistencia))
+      (td ,(number->string prevencao_erros))
+      (td ,(number->string tolerancia_falhas))
+      (td ,(number->string flexibilidade_eficiencia))
+      (td ,(number->string estetica_design))
+      (td ,(number->string expectativa))
+      (td ,(number->string normas_convencao))
       (td ,comentarios)
       (td ,avaliado-em))))
 
@@ -88,9 +118,9 @@
        (error (format "Valor inválido em ~a: não é string" chave))]
       [else
        (define num (string->number str))
-       (if (and num (integer? num))
+       (if (and (and num (integer? num) (and (> num -1) (< num 11))))
            num
-           (error (format "Valor inválido em ~a: deve ser número inteiro" chave)))])))
+           (error (format "Valor inválido em ~a: deve ser número inteiro entre 0 e 10" chave)))])))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -115,10 +145,20 @@
         (body
           (h1 "Avaliação de Sites - NBR 9241-11")
           (form ([action "/submit"] [method "post"])
-            (p "Site URL: " (input ([type "text"] [name "site"] [required "true"])))
-            (p "Eficácia (0-10): " (input ([type "number"] [name "eficacia"] [min "0"] [max "10"] [required "true"])))
-            (p "Eficiência (0-10): " (input ([type "number"] [name "eficiencia"] [min "0"] [max "10"] [required "true"])))
-            (p "Satisfação (0-10): " (input ([type "number"] [name "satisfacao"] [min "0"] [max "10"] [required "true"])))
+            (p "Site: " (input ([type "text"] [name "site"] [required "true"])))
+            (p "URL: " (input ([type "text"] [name "url"] [required "true"])))
+            (p "Tipo de usuários tester's: " (input ([type "text"] [name "tipo_usuario"] [required "true"])))
+            (p "Componentes testados: " (input ([type "text"] [name "componente_avaliado"] [required "true"])))
+            (p "Adequação ao uso (0-10): " (input ([type "number"] [name "adequacao"] [min "0"] [max "10"] [required "true"])))
+            (p "Auto descrição (0-10): " (input ([type "number"] [name "descricao"] [min "0"] [max "10"] [required "true"])))
+            (p "Controle explícito (0-10): " (input ([type "number"] [name "explicito"] [min "0"] [max "10"] [required "true"])))
+            (p "Consistência (0-10): " (input ([type "number"] [name "consistencia"] [min "0"] [max "10"] [required "true"])))
+            (p "Prevenção de erros (0-10): " (input ([type "number"] [name "prevencao_erros"] [min "0"] [max "10"] [required "true"])))
+            (p "Tolerância a falhas (0-10): " (input ([type "number"] [name "tolerancia_falhas"] [min "0"] [max "10"] [required "true"])))
+            (p "Flexibilidade e eficiência de uso (0-10): " (input ([type "number"] [name "flexibilidade_eficiencia"] [min "0"] [max "10"] [required "true"])))
+            (p "Estética e design minimalista (0-10): " (input ([type "number"] [name "estetica_design"] [min "0"] [max "10"] [required "true"])))
+            (p "Compatibilidade com a expectativa dos usuários (0-10): " (input ([type "number"] [name "expectativa"] [min "0"] [max "10"] [required "true"])))
+            (p "Conformidade com as normas e convenções (0-10): " (input ([type "number"] [name "normas_convencao"] [min "0"] [max "10"] [required "true"])))
             (p "Comentários: " (textarea ([name "comentarios"] [required "true"])))
             (p (input ([type "submit"] [value "Enviar Avaliação"])))
           )
@@ -139,14 +179,24 @@
     ;;          valores)
 
       ;; Converte para string e número
-      (define site       (assoc-ref valores 'site))
-      (define eficacia   (string->number (assoc-ref valores 'eficacia)))
-      (define eficiencia (string->number (assoc-ref valores 'eficiencia)))
-      (define satisfacao (string->number (assoc-ref valores 'satisfacao)))
+      (define site                      (assoc-ref valores 'site))
+      (define url                       (assoc-ref valores 'url))
+      (define tipo_usuario              (assoc-ref valores 'tipo_usuario))
+      (define componente_avaliado       (assoc-ref valores 'componente_avaliado))
+      (define adequacao                 (string->number (assoc-ref valores 'adequacao)))
+      (define descricao                 (string->number (assoc-ref valores 'descricao)))
+      (define explicito                 (string->number (assoc-ref valores 'explicito)))
+      (define consistencia              (string->number (assoc-ref valores 'consistencia)))
+      (define prevencao_erros           (string->number (assoc-ref valores 'prevencao_erros)))
+      (define tolerancia_falhas         (string->number (assoc-ref valores 'tolerancia_falhas)))
+      (define flexibilidade_eficiencia  (string->number (assoc-ref valores 'flexibilidade_eficiencia)))
+      (define estetica_design           (string->number (assoc-ref valores 'estetica_design)))
+      (define expectativa               (string->number (assoc-ref valores 'expectativa)))
+      (define normas_convencao          (string->number (assoc-ref valores 'normas_convencao)))
       (define comentarios (assoc-ref valores 'comentarios))
 
       ;; Insere no banco
-      (inserir-avaliacao site eficacia eficiencia satisfacao comentarios)
+      (inserir-avaliacao site url tipo_usuario componente_avaliado adequacao descricao explicito consistencia prevencao_erros tolerancia_falhas flexibilidade_eficiencia estetica_design expectativa normas_convencao comentarios)
 
       (response/xexpr
         `(html
@@ -168,8 +218,7 @@
           (h1 "Avaliações Registradas")
           (table ([border "1"])
             (tr
-              (th "ID") (th "Site") (th "Eficácia") (th "Eficiência")
-              (th "Satisfação") (th "Comentários") (th "Data"))
+              (th "ID") (th "Site") (th "Url") (th "Tipo de usuario") (th "Componente avaliado") (th "Adequação ao uso") (th "Auto descrição") (th "Controle explícito") (th "Consistência") (th "Prevenção de erros") (th "Tolerância a falhas") (th "Flexibilidade") (th "Estética") (th "Compatibilidade e expectativa") (th "Normas e convenções") (th "Comentarios extra") (th "Data"))
             ,@(gerar-linhas avaliacoes))
           (p (a ([href "/"]) "Voltar ao formulário"))
         )))]
@@ -187,8 +236,7 @@
              (h1 "Resultados da Pesquisa")
              (table ([border "1"])
                (tr
-                 (th "ID") (th "Site") (th "Eficácia") (th "Eficiência")
-                 (th "Satisfação") (th "Comentários") (th "Data"))
+                 (th "ID") (th "Site") (th "Url") (th "Tipo de usuario") (th "Componente avaliado") (th "Adequação ao uso") (th "Auto descrição") (th "Controle explícito") (th "Consistência") (th "Prevenção de erros") (th "Tolerância a falhas") (th "Flexibilidade") (th "Estética") (th "Compatibilidade e expectativa") (th "Normas e convenções") (th "Comentarios extra") (th "Data"))
                ,@(gerar-linhas avaliacoes))
              (p (a ([href "/"]) "Voltar ao formulário"))
            )))
